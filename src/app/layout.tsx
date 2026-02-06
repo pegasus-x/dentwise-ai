2a34bdde5d42c9dab7dfe9cd399bd7c1de9d7bd8
Rati Ranjan Sendha <135224384+pegasus-x@users.noreply.github.com>
2026-02-06 15:44:22 +0530

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import {ClerkProvider} from '@clerk/nextjs'
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DentWise - AI Powered Dental Assistant",
+  description: "Get instant dental advice and support with DentWise, your AI-powered dental assistant. Ask questions, receive personalized recommendations, and manage your oral health with ease.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+    </ClerkProvider> 
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignIn, SignOutButton, SignUpButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <SignedOut>
+        <SignUpButton mode="modal">Sign Up</SignUpButton> 
+      </SignedOut>
+      
+      <SignedIn>
+        <SignOutButton>Log Out</SignOutButton>
+      </SignedIn>
+    </div>
+  );
+}
