a7493df5bcba6595f2749e28e33c3ce019b7aa39
Rati Ranjan Sendha <135224384+pegasus-x@users.noreply.github.com>
2026-04-10 20:19:17 +0530

    Add dynamic export to DashboardPage component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import MainActions from "@/components/dashboard/MainActions";
